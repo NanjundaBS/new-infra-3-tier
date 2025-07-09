defe122830b575ad3a63d614126679e9c4eb3834
NanjundaBS <71710547+NanjundaBS@users.noreply.github.com>
2025-07-09 14:20:10 +0530

    Update appasg.tf

--- appasg.tf
+++ appasg.tf
@@ -22,7 +22,7 @@ resource "aws_launch_template" "swiggy-app-template" {
   name_prefix   = "swiggy-app-template"
   image_id      = "ami-000ec6c25978d5999"
   instance_type = "t2.micro"
-  key_name      = "devopsbynanju.pem"
+  key_name      = "N.virginia.pem"
 
   network_interfaces {
     security_groups             = [aws_security_group.swiggy-ec2-asg-sg-app.id]
